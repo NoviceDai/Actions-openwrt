9302b6547595082460a1cf61238654bb89dff34a
NoviceDai <daixiao128@163.com>
2023-06-03 10:47:25 +0800

    Luci footer changed.

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,14 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# Modify Dropbear Welcome banner..
 sed -i "s/%D %V, %C/%D %V, Compile at $(date +'%Y.%m.%d') by DaiXiao/g" package/base-files/files/etc/banner
 
 # Modify firmware information 
 #can not find the file , maybe too early??? 
 # sed -i '23a distversion = "DX Compiled at $(date +"%Y.%m.%d-%H:%M")"'  feeds/luci/modules/luci-lua-runtime/src/mkversion.sh
+
+# Modify theme material--footer ,add--"compile by daixiao"
+sed -i "s/https:\/\/github.com\/openwrt\/luci/https:\/\/github.com\/NoviceDai\/openwrt/g"  feeds\luci\themes\luci-theme-material\ucode\template\themes\material\footer.ut
+sed -i "s/Powered by {{ version.luciname }} ({{ version.luciversion }})</a> \//Compiled by DaiXiao at $(date +'%Y.%m.%d')/g)</a>"  feeds\luci\themes\luci-theme-material\ucode\template\themes\material\footer.ut
+sed -i "s/{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }})/.../g"  feeds\luci\themes\luci-theme-material\ucode\template\themes\material\footer.ut
