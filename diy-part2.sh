e1a8a11db05ef8f200171383d275148dcbe4339f
NoviceDai <daixiao128@163.com>
2023-06-03 11:38:27 +0800

    diy2 bugs fixed

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,6 +20,6 @@ sed -i "s/%D %V, %C/%D %V, Compile at $(date +'%Y.%m.%d') by DaiXiao/g" package/
 # sed -i '23a distversion = "DX Compiled at $(date +"%Y.%m.%d-%H:%M")"'  feeds/luci/modules/luci-lua-runtime/src/mkversion.sh
 
 # Modify theme material--footer ,add--"compile by daixiao"
-sed -i "s/https:\/\/github.com\/openwrt\/luci/https:\/\/github.com\/NoviceDai\/openwrt/g"  feeds\luci\themes\luci-theme-material\ucode\template\themes\material\footer.ut
-sed -i "s/Powered by {{ version.luciname }} ({{ version.luciversion }})</a> \//Compiled by DaiXiao at $(date +'%Y.%m.%d')/g)</a>"  feeds\luci\themes\luci-theme-material\ucode\template\themes\material\footer.ut
-sed -i "s/{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }})/.../g"  feeds\luci\themes\luci-theme-material\ucode\template\themes\material\footer.ut
+sed -i "s/https:\/\/github.com\/openwrt\/luci/https:\/\/github.com\/NoviceDai\/openwrt/g"  feeds/luci/themes/luci-theme-material/ucode/template/themes/material/footer.ut
+sed -i "s/Powered by {{ version.luciname }} ({{ version.luciversion }})</a> \//Compiled by DaiXiao at $(date +'%Y.%m.%d')/g)</a>"  feeds/luci/themes/luci-theme-material/ucode/template/themes/material/footer.ut
+sed -i "s/{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }})/.../g"  feeds/luci/themes/luci-theme-material/ucode/template/themes/material/footer.ut
