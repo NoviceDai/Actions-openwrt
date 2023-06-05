48f27de00d0659d55d5cf3b6ec0e5125cd8d3322
NoviceDai <daixiao128@163.com>
2023-06-05 13:11:45 +0800

    modify dropbear banner

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,13 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # Modify Dropbear Welcome banner..
-sed -i "s/%D %V, %C/%D %V, Compiled at $(date +'%Y.%m.%d') by DaiXiao/g" package/base-files/files/etc/banner
 
+cat <<EOF >package/base-files/files/etc/banner
+ ___       _ __  _ _           
+| . \ ___ <_>\ \/ <_> ___  ___ 
+| | |<_> || | \ \ | |<_> |/ . \      ~~
+|___/<___||_|_/\_\|_|<___|\___/    ()
+------------------------------------------------------
+ %D %V, Compiled at $(date +'%Y.%m.%d') by Daixiao
+------------------------------------------------------
+EOF
