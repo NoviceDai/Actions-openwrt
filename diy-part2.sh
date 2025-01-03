151356e6e051d95c38908b3aeee2e9c74abd45ea
NoviceDai <133297968+NoviceDai@users.noreply.github.com>
2025-01-03 13:53:32 +0800

    尝试v23编译
    尝试v23编译

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,7 +5,7 @@
 #
 
 #Tor ...change default ver 0.4.7.19/...to ver 0.4.0.4.8.1-alpha 
-sed -i "/PKG_VERSION:/cPKG_VERSION:=0.4.8.1-alpha"  feeds/packages/net/tor/Makefile
-sed -i "/PKG_HASH:/cPKG_HASH:=f73719a8cec65ce473fae19485b3b89e767a26b20fa9e172052df2cfd4f92e51"  feeds/packages/net/tor/Makefile
+#sed -i "/PKG_VERSION:/cPKG_VERSION:=0.4.8.1-alpha"  feeds/packages/net/tor/Makefile
+#sed -i "/PKG_HASH:/cPKG_HASH:=f73719a8cec65ce473fae19485b3b89e767a26b20fa9e172052df2cfd4f92e51"  feeds/packages/net/tor/Makefile
 
 #End 
