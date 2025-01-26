eb1be7e2258f77f9e77bb59e44466626ef02fc2e
NoviceDai <daixiao128@163.com>
2025-01-26 17:32:50 +0800

    update plugins

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,7 @@
 
 #echo 'src-git smpackage  https://github.com/kenzok8/small-package.git' >>feeds.conf.default
 #echo 'src-git daixiao https://github.com/NoviceDai/OpenwrtAddon.git' >>feeds.conf.default
+
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
